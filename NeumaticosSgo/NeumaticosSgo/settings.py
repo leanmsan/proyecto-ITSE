26c2778a26c8f76b3df62957c1d35967a631c909
Leandro Sánchez <leanmsan@gmail.com>
2023-05-16 17:30:32 -0300

    Proyecto
    Signed-off-by: Leandro Sánchez <leanmsan@gmail.com>

--- NeumaticosSgo/NeumaticosSgo/settings.py
+++ NeumaticosSgo/NeumaticosSgo/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'NeumaticosSgo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'NeumaticosSgo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
      'default': {
-         'ENGINE': 'django.db.backends.mysql',
-         'NAME': 'NeumaticosSgo',
-         'USER': 'admin',
-         'PASSWORD': 'admin',
-         'HOST': '127.0.0.1',
-         'PORT': '3306',
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.mysql',
+#          'NAME': BASE_DIR / 'NeumaticosSgo',
+#          'USER': 'admin',
+#          'PASSWORD': 'admin',
+#          'HOST': '127.0.0.1',
+#          'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
