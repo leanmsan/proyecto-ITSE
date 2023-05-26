d7a07a74d446acdb4cb8e23b0068a10393dfd934
Jorge Nicolas Moises <86451624+nicolasmoises@users.noreply.github.com>
2023-05-26 19:43:17 -0300

    creacion api productos get

--- NeumaticosSgo/productos/views.py
+++ NeumaticosSgo/productos/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from forms import ProductoForm
+#from forms import ProductoForm
 from common.models import Producto, Bateria, Filtro, Lampara, Llanta, Lubricentro, Neumatico
-
+from django.views import View
+from django.http.response import JsonResponse
 # Create your views here.
-
+'''
 def add_producto(request):
     if request.method == 'POST':
         form = ProductoForm(request.POST)
@@ -17,3 +18,20 @@ def add_producto(request):
 def listado_Productos(request):
     productos = Producto.objects.all()
     return render(request, 'listadoProductos.html', {'productos': productos})
+'''
+class ProductoView(View):
+    def get(self,request):
+        productos = list(Producto.objects.values())
+        print(productos)
+        if len(productos) > 0:
+            datos = {'mensaje': 'exito', 'productos': productos}
+        else:
+            datos = {'mensaje': 'no se encuentra productos'}
+        return JsonResponse(datos)
+
+    def post(self,request):
+        pass
+    def put(self,request):
+        pass
+    def delete(self,request):
+        pass
