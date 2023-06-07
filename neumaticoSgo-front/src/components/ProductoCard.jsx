97eb0f7dd0ff0dae20defb4ecbcc53e5a99a5e17
aqulesz <franmoyano123@gmail.com>
2023-06-07 17:54:02 -0300

    correccion de errores con nueva db

--- neumaticoSgo-front/src/components/ProductoCard.jsx
+++ neumaticoSgo-front/src/components/ProductoCard.jsx
@@ -1,12 +1,12 @@
 export function ProductoCard({producto}) {
     return (
         <div>
-        <h1>Producto: {producto.nombreproducto}</h1>
-        <p>Marca del Producto: {producto.marcaproducto}</p>
+        <h1>Producto: {producto.nombre}</h1>
+        <p>Marca del Producto: {producto.marca}</p>
         <p>Precio Compra: {producto.preciocompra}</p>
         <p>Precio Venta: {producto.precioventa}</p>
-        <p>Stock Producto: {producto.stockproducto}</p>
-        <p>Rubro Producto: {producto.rubroproducto}</p>
+        <p>Stock Producto: {producto.stock}</p>
+        <p>Rubro Producto: {producto.rubro}</p>
         <hr />
     </div>
     )
