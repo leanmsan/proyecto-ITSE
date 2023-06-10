89950ebd01ea8659071774d2c352a9e9e78739b3
aqulesz <franmoyano123@gmail.com>
2023-06-10 20:28:30 -0300

    correccion-front

--- NeumaticosSgo/NeumaticosSgo/settings.py
+++ NeumaticosSgo/NeumaticosSgo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'corsheaders',
     'rest_framework',
     'common',
--- neumaticoSgo-front/src/components/ProductoCard.jsx
+++ neumaticoSgo-front/src/components/ProductoCard.jsx
@@ -1,4 +1,15 @@
 export function ProductoCard({producto}) {
+
+    const caracteristicaString = JSON.stringify(producto.caracteristicas);
+    const caracteristicasLimpias = caracteristicaString.replace(/[^a-zA-Z0-9]/g, ' ');
+
+    function convertirCapitalCase(texto) {
+        return texto.toLowerCase().replace(/(?:^|\s)\w/g, function(letra) {
+          return letra.toUpperCase();
+        });   
+    }
+    
+    const caracteristicasFinal = convertirCapitalCase(caracteristicasLimpias);
     return (
         <div>
         <h1>Producto: {producto.nombre}</h1>
@@ -9,7 +20,7 @@ export function ProductoCard({producto}) {
         <p>Precio Venta: {producto.precioventa}</p>
         <p>Stock Producto: {producto.stockdisponible}</p>
         <p>Rubro: {producto.rubro_id}</p>
-        <p>Caracteristicas: {JSON.stringify(producto.caracteristicas)}</p>
+        <p>Caracteristicas: {caracteristicasFinal}</p>
         <hr />
     </div>
     )
