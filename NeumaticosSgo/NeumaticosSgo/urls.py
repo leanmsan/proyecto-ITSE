cecd1e1118603e354db4b0b82ce4d3a4dd11be9a
Leandro Sánchez <leanmsan@gmail.com>
2023-07-06 18:26:31 -0300

    Correccion de mi cagada 3
    Signed-off-by: Leandro Sánchez <leanmsan@gmail.com>

---
+++ NeumaticosSgo/NeumaticosSgo/urls.py
@@ -0,0 +1,34 @@
+"""
+URL configuration for NeumaticosSgo project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path('', include('login.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    #path('accounts/logout', include('login.urls'), name='logout'),
+    path('api/', include('productos.urls'), name='productos'),
+    path('api/', include('rubros.urls'),name='rubros'),
+    path('api/', include('proveedores.urls'),name='proveedores'),
+    path('api/', include('movimientos.urls'),name='movimientos'),
+    path('api/token/', obtain_auth_token, name='api_token_auth'),
+    path('api/', include('apiLogin.urls')),
+    path('api2/', include('apiLogin2.urls'))
+
+]
